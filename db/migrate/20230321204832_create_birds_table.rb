1ec645df9bc438c86b3d01a65e4d1167687630ed
Jianqiu Wang <jianqiuwang1@icloud.com>
2023-03-21 15:50:21 -0500

    fixed the issue

---
+++ db/migrate/20230321204832_create_birds_table.rb
@@ -0,0 +1,9 @@
+class CreateBirdsTable < ActiveRecord::Migration[7.0]
+  def change
+    create_table :birds do |t|
+      t.string :name
+      t.string :species
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_18_202252) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_21_204832) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
