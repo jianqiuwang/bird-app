4d285e986bfe49be719e37e247884af1c915a87d
Jianqiu Wang <jianqiuwang1@icloud.com>
2023-03-21 15:29:53 -0500

    test deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Bird.destroy_all()
 Bird.create!(name: 'Black-Capped Chickadee', species: 'Poecile Atricapillus')
 Bird.create!(name: 'Grackle', species: 'Quiscalus Quiscula')
 Bird.create!(name: 'Common Starling', species: 'Sturnus Vulgaris')
